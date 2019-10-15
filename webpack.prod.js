a2b15cffc9fbff236a06202b83840d4ea1f45bf0
Sal Tamay <sal.tamay@outlook.com>
2019-10-14 21:45:34 -0400

    Add Service Workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HTMLWebPackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/public/js/index.js',
@@ -11,6 +12,24 @@ module.exports = {
         test: '/\.js$/',
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.(jpg|png|svg|jpg|gif)$/,
+        loader: 'file-loader'
       }
     ]
   },
@@ -18,7 +37,7 @@ module.exports = {
     new HTMLWebPackPlugin({
       template: './src/public/index.html',
       filename: 'index.html'
-    }
-    )
+    }),
+    new WorkboxPlugin.GenerateSW()
   ]
 }
